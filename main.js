ae7f3903176e9c37d6095d85bd6a2d11e8772bbd
coloradokim <schlesinger.kim@gmail.com>
2017-08-15 19:24:12 -0600

    one get request works

--- main.js
+++ main.js
@@ -1,55 +1,37 @@
 var google = require('googleapis');
 var sheets = google.sheets('v4');
-require('dotenv').config()
+require('dotenv').config();
 
 var request = {
-  // The spreadsheet to request.
   spreadsheetId: '1yVuMt6LMTlq-4cyvVTvq776Dj0wjJjEulbaX_qWriVs',
-  // The ranges to retrieve from the spreadsheet.
-  ranges: ["R1C1:R50C10"],
-  // True if grid data should be returned.
-  // This parameter is ignored if a field mask was set in the request.
+  ranges: 'R1C1:R10C10',
   includeGridData: false,
   auth: process.env.API_KEY
 };
 
 // GET data from Learn Mapping Sheet ('Learn Mapping')
-sheets.spreadsheets.get(request, function(err, response) {
+
+// sheets.spreadsheets.get(request, function(err, response) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     let learnMappingSheet = response.sheets.find(function(sheet) {
+//       return sheet.properties.title === 'Learn Mapping'
+//     });
+//     console.log('Learn Mapping Info:' + learnMappingSheet);
+//   };
+// });
+
+
+// GET data from survey responses ('Form Responses 1')
+sheets.spreadsheets.values.get({
+  spreadsheetId: '1yVuMt6LMTlq-4cyvVTvq776Dj0wjJjEulbaX_qWriVs',
+  range: 'R1C1:R40C10',
+  auth: process.env.API_KEY,
+}, function(err, result) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-     console.log(response.sheets);
-      let learnMappingSheet = response.sheets.find(function(sheet) {
-          return sheet.properties.title === 'Learn Mapping'
-        });
-        // console.log(learnMappingSheet);
-    };
+    console.log(result);
+  }
 });
-
-
-    //Original request that worked.
-    // sheets.spreadsheets.get(request, function(err, response) {
-    //   if (err) {
-    //     console.error(err);
-    //   } else {
-    //     let learnMappingSheet = response.sheets.find(function(sheet) {
-    //       return sheet.properties.title === 'Learn Mapping'
-    //     });
-    //     console.log('Learn Mapping Info:' + learnMappingSheet);
-    //   };
-    // });
-
-    // GET data from survey responses ('Form Responses 1')
-    // sheets.spreadsheets.values.get({
-    //    spreadsheetId: '1yVuMt6LMTlq-4cyvVTvq776Dj0wjJjEulbaX_qWriVs',
-    //    range: "R1C1:R40C10",
-    //    auth: process.env.API_KEY,
-    // }, function(err, result) {
-    //   if(err) {
-    //     // Handle error
-    //     console.log(err);
-    //   } else {
-    //     var numRows = result.values ? result.values.length : 0;
-    //     console.log(result);
-    //   }
-    // });
