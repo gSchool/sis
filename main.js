75527a6a71f227a3e39e7b47dc718a87ffcdc1e9
coloradokim <schlesinger.kim@gmail.com>
2017-08-15 18:46:55 -0600

    remove test branch

--- main.js
+++ main.js
@@ -5,9 +5,8 @@ require('dotenv').config()
 var request = {
     // The spreadsheet to request.
     spreadsheetId: '1yVuMt6LMTlq-4cyvVTvq776Dj0wjJjEulbaX_qWriVs',  // TODO: Update placeholder value.
-
     // The ranges to retrieve from the spreadsheet.
-    // ranges: [],  // TODO: Update placeholder value.
+    ranges: ["R1C1:R50C10"],
 
     // True if grid data should be returned.
     // This parameter is ignored if a field mask was set in the request.
@@ -15,13 +14,28 @@ var request = {
     auth: process.env.API_KEY
   };
 
-sheets.spreadsheets.get(request, function(err, response) {
-  if (err) {
-    console.error(err);
+// sheets.spreadsheets.get(request, function(err, response) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     let learnMappingSheet = response.sheets.find(function(sheet) {
+//       return sheet.properties.title === 'Learn Mapping'
+//     })
+//     console.log(learnMappingSheet);
+//   }
+// });
+
+// GET data from survey responses ('Form Responses 1')
+sheets.spreadsheets.values.get({
+   spreadsheetId: '1yVuMt6LMTlq-4cyvVTvq776Dj0wjJjEulbaX_qWriVs',
+   range: "R1C1:R40C10",
+   auth: process.env.API_KEY,
+}, function(err, result) {
+  if(err) {
+    // Handle error
+    console.log(err);
   } else {
-    let learnMappingSheet = response.sheets.find(function(sheet) {
-      return sheet.properties.title === 'Learn Mapping'
-    })
-    console.log(learnMappingSheet);
+    var numRows = result.values ? result.values.length : 0;
+    console.log(result);
   }
 });
